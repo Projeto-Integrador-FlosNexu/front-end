118444bc32407a37e6a740f0b81bf313f454d76e
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-06 10:38:35 -0300

    feat(home): update Home.tsx with better classnames and imports
    Remove unused 'useNavigate' import from Home.tsx, Cadastro.tsx, and Login.tsx.  Also standardize quotations and remove extra line spaces in Home.tsx for cleaner, more consistent code style. No functional changes introduced.
    
    - The 'useNavigate' hook was imported but never used in these files, suggesting a cleanup of the codebase.
    - Quotation marks were standardized to double quotes for consistency.
    - Excess line spaces were removed for compactness and readability.
    - Changes are purely code styling related and no functionality should be affected.
    - This change does not introduce any breaking changes or new features.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import './Home.css';
-import homeLogo from '../../assets/react.svg'
-import { Link, useNavigate } from 'react-router-dom';
-import Carousel from '../../components/swiperCorousel/CarouselContainer';
+import "./Home.css";
+import homeLogo from "../../assets/react.svg";
+import { Link } from "react-router-dom";
+import Carousel from "../../components/swiperCorousel/CarouselContainer";
 
 function Home() {
-    return (
-        <>
-          
-          {Carousel()}
-        <div className="bg-[#228B22] flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
+  return (
+    <>
+      {Carousel()}
+      <div className="bg-[#228B22] flex justify-center">
+        <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem-vinde:</h2>
-              <Link to="/login" className='rounded bg-[#F5DABF] text-[#FF8100] py-2 px-4'>Voltar</Link>
-              <Link to="/cadastroProduto" className='rounded bg-[#F5DABF] text-[#FF8100] py-2 px-4'>Cadastrar Produto</Link>
-            </div>
-            <div className="flex justify-center ">
-              <img src={homeLogo} alt="" className='w-2/3' />
-      
-            </div>
-      
-            </div>
+            <h2 className="text-5xl font-bold">Seja bem-vinde:</h2>
+            <Link
+              to="/login"
+              className="rounded bg-[#F5DABF] text-[#FF8100] py-2 px-4"
+            >
+              Voltar
+            </Link>
+            <Link
+              to="/cadastroProduto"
+              className="rounded bg-[#F5DABF] text-[#FF8100] py-2 px-4"
+            >
+              Cadastrar Produto
+            </Link>
           </div>
-       
-      
-      </>
-    );
+          <div className="flex justify-center ">
+            <img src={homeLogo} alt="" className="w-2/3" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
