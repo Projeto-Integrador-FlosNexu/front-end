56d42238fca867267bc439a9ef2bb8d258a1e817
felipe-jaque-dev <felipealexandrej@gmail.com>
2024-02-06 12:28:37 -0300

    Fix: Carrousel/home/

--- src/components/swiperCorousel/Slide02.tsx
+++ src/components/swiperCorousel/Slide02.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './CarouselStyle.css';
 
 function Slide02() {
@@ -30,10 +31,10 @@ function Slide02() {
                             font-bold
                             text-white/80
                             '>
-                        Seja bem vinde!
+                        Seja bem vindos!
                     </h2>
                     <p className='text-xl text-white/80'  >
-                        Aqui você encontra os melhores Games!
+                        Aqui você encontra os melhores:!
                         </p>
 
                     <div className="flex justify-around gap-4">
@@ -51,6 +52,17 @@ function Slide02() {
                                     '>
                             Conheça mais
                         </button>
+                    </div>
+                    <div className="flex justify-around gap-4">
+                    <Link to='/login' className='rounded
+                                    bg-transparent
+                                    border
+                                    border-white/50
+                                    text-white/50
+
+                                    hover:border-white/80
+                                    hover:text-white/80
+                                    py-2 px-2 '>Entrar <br /> temporario</Link>
                     </div>
                 </div>
 
