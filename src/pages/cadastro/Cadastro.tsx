bce16839234ed7713d644522a14b77cab2b7b05f
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-02-06 16:01:59 -0300

    Atualizando

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Usuario from '../../models/Usuario'
 import { cadastrarUsuario } from '../../services/Service'
@@ -71,107 +71,107 @@ function Cadastro() {
 
   return (
     <>
-        <div className="fundoCadastro ">
-          <div className='conteudo'>
-            <div className='form-boxCadastro'>
-              <form onSubmit={cadastrarNovoUsuario}>
-                <h2 className='text-white text-4xl mb-3 ml-'>Cadastrar</h2>
-                <div className='input-containerCadastro'>
-                  <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204321146200264744/user_1.png?ex=65d44e78&is=65c1d978&hm=67ab9c2abc91dfa403b2f67d5dd76630abf6ec2a1c6ea39db2536a4c89ef246c&' />
-                  <div className="inputboxCadastro">
-                    <input
-                      type="text"
-                      id="nome"
-                      name="nome"
-                      placeholder="Nome"
-                      className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
-                      value={usuario.nome}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                  </div>
+      <div className="fundoCadastro ">
+        <div className='conteudo'>
+          <div className='form-boxCadastro'>
+            <form onSubmit={cadastrarNovoUsuario}>
+              <h2 className='text-white text-4xl mb-3 ml-'>Cadastrar</h2>
+              <div className='input-containerCadastro'>
+                <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204321146200264744/user_1.png?ex=65d44e78&is=65c1d978&hm=67ab9c2abc91dfa403b2f67d5dd76630abf6ec2a1c6ea39db2536a4c89ef246c&' />
+                <div className="inputboxCadastro">
+                  <input
+                    type="text"
+                    id="nome"
+                    name="nome"
+                    placeholder="Nome"
+                    className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
+                    value={usuario.nome}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                  />
                 </div>
-                <div className='input-containerCadastro'>
-                  <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204321565743915041/envelope-simple_1.png?ex=65d44edc&is=65c1d9dc&hm=902287b6d2b600a336afa258d4c592610b8aab709885c626ceb97f5983acfd0f&' />
-                  <div className="inputboxCadastro">
-                    <input
-                      type="text"
-                      id="email"
-                      name="email"
-                      placeholder="Seu Email"
-                      className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
-                      value={usuario.email}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                  </div>
+              </div>
+              <div className='input-containerCadastro'>
+                <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204321565743915041/envelope-simple_1.png?ex=65d44edc&is=65c1d9dc&hm=902287b6d2b600a336afa258d4c592610b8aab709885c626ceb97f5983acfd0f&' />
+                <div className="inputboxCadastro">
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Seu Email"
+                    className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
+                    value={usuario.email}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                  />
                 </div>
-                <div className='input-containerCadastro'>
-                  <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204323382657552394/identification-card.png?ex=65d4508d&is=65c1db8d&hm=3c962330883bd1fc71fe71ed9f90798fac9c2a858056bf8b6b87b799732975a9&' />
-                  <div className="inputboxCadastro">
-                    <input
-                      type="text"
-                      id="tipo"
-                      name="tipo"
-                      placeholder="Tipo de Usuario"
-                      className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
-                      value={usuario.tipo}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                  </div>
+              </div>
+              <div className='input-containerCadastro'>
+                <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204323382657552394/identification-card.png?ex=65d4508d&is=65c1db8d&hm=3c962330883bd1fc71fe71ed9f90798fac9c2a858056bf8b6b87b799732975a9&' />
+                <div className="inputboxCadastro">
+                  <input
+                    type="text"
+                    id="tipo"
+                    name="tipo"
+                    placeholder="Tipo de Usuario"
+                    className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
+                    value={usuario.tipo}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                  />
                 </div>
-                <div className='input-containerCadastro'>
-                  <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204323543480008755/camera.png?ex=65d450b4&is=65c1dbb4&hm=3e0368cd2d70e83214aa29a57fe68085613352f96af3397f30fe165a628f43d2&' />
-                  <div className="inputboxCadastro">
-                    <input
-                      type="text"
-                      id="foto"
-                      name="foto"
-                      placeholder="Foto"
-                      className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
-                      value={usuario.foto}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                  </div>
+              </div>
+              <div className='input-containerCadastro'>
+                <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204323543480008755/camera.png?ex=65d450b4&is=65c1dbb4&hm=3e0368cd2d70e83214aa29a57fe68085613352f96af3397f30fe165a628f43d2&' />
+                <div className="inputboxCadastro">
+                  <input
+                    type="text"
+                    id="foto"
+                    name="foto"
+                    placeholder="Foto"
+                    className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
+                    value={usuario.foto}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                  />
                 </div>
-                <div className='input-containerCadastro'>
-                  <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204323683972161566/image.png?ex=65d450d5&is=65c1dbd5&hm=e266b401479af7fab9d6a75aefb0d5d69dc8f72194a37d1c63b6caad82dc75ac&' />
-                  <div className="inputboxCadastro">
-                    <input
-                      type="password"
-                      id="senha"
-                      name="senha"
-                      placeholder="Senha"
-                      className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
-                      value={usuario.senha}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                  </div>
+              </div>
+              <div className='input-containerCadastro'>
+                <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204323683972161566/image.png?ex=65d450d5&is=65c1dbd5&hm=e266b401479af7fab9d6a75aefb0d5d69dc8f72194a37d1c63b6caad82dc75ac&' />
+                <div className="inputboxCadastro">
+                  <input
+                    type="password"
+                    id="senha"
+                    name="senha"
+                    placeholder="Senha"
+                    className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
+                    value={usuario.senha}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                  />
                 </div>
-                <div className='input-containerCadastro'>
-                  <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204324906750320640/lock-laminated.png?ex=65d451f9&is=65c1dcf9&hm=e9553807fdc65fb4209ba7ce990feac2d5120a81e8b96914ec7080c87c5546fe&' />
-                  <div className="inputboxCadastro">
-                    <input
-                      type="password"
-                      id="confirmarSenha"
-                      name="confirmarSenha"
-                      placeholder="Confirmar Senha"
-                      className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
-                      value={confirmaSenha}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
-                    />
-                  </div>
+              </div>
+              <div className='input-containerCadastro'>
+                <img src='https://cdn.discordapp.com/attachments/1202676755547037716/1204324906750320640/lock-laminated.png?ex=65d451f9&is=65c1dcf9&hm=e9553807fdc65fb4209ba7ce990feac2d5120a81e8b96914ec7080c87c5546fe&' />
+                <div className="inputboxCadastro">
+                  <input
+                    type="password"
+                    id="confirmarSenha"
+                    name="confirmarSenha"
+                    placeholder="Confirmar Senha"
+                    className="placeholderCadastro rounded-3xl border-2 border-slate-700 p-2"
+                    value={confirmaSenha}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
+                  />
                 </div>
-                <div className="flex justify-around w-full gap-8">
-                  <button className='buttonCadastro rounded-3xl text-black bg-white w-4/1 py-1.5 flex justify-center' onClick={back}>
-                    Cancelar
-                  </button>
-                  <button className='buttonCadastro rounded-3xl text-black bg-white w-4/1 py-1.5 flex justify-center' type='submit'>
-                    Cadastrar
-                  </button>
-                </div>
-              </form>
-            </div>
+              </div>
+              <div className="flex justify-around w-full gap-8">
+                <button className='buttonCadastro rounded-3xl text-black bg-white w-4/1 py-1.5 flex justify-center' onClick={back}>
+                  Cancelar
+                </button>
+                <button className='buttonCadastro rounded-3xl text-black bg-white w-4/1 py-1.5 flex justify-center' type='submit'>
+                  Cadastrar
+                </button>
+              </div>
+            </form>
           </div>
         </div>
+      </div>
     </>
   )
 }
