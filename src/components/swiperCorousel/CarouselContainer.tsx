59b1bc0542c472e7586d87ee7c12a4f3c7afc435
felipe-jaque-dev <felipealexandrej@gmail.com>
2024-02-06 19:52:56 -0300

    Adicionado menu

--- src/components/swiperCorousel/CarouselContainer.tsx
+++ src/components/swiperCorousel/CarouselContainer.tsx
@@ -32,7 +32,7 @@ function Carousel() {
                         
                     <Link to='/produtos' className='hover:underline'>
                         <img
-                            src='https://i.imgur.com/JKqFVzv.png'
+                            src='https://www.portalsolar.com.br/_next/image?url=%2Fassets%2Fsimulator%2Fenergia-solar-residencial-portal-solar.webp&w=2048&q=75'
                             alt="Slider"
                             className='slide-item'
                         />
@@ -41,13 +41,13 @@ function Carousel() {
                     <SwiperSlide>
                     <Link to='/produtos' className='hover:underline'>
                         <img
-                            src='https://i.imgur.com/xLudfgf.png'
+                            src='https://cdn.motor1.com/images/mgl/Vz8EEz/s1/renault-zoe---um-mes-dirigindo-um-carro-eletrico-4.webp'
                             alt="Slider"
                             className='slide-item'
                         />
                     </Link>
                     </SwiperSlide>
-
+                    
                     <SwiperSlide>
                     <Link to='/produtos' className='hover:underline'>
                         <img
--- src/components/swiperCorousel/Slide02.tsx
+++ src/components/swiperCorousel/Slide02.tsx
@@ -4,71 +4,29 @@ import './CarouselStyle.css';
 function Slide02() {
     return (
 
-        <div className="bg-image-slide02
-                swiper-slide
-                flex 
-                justify-center
-                
-                ">
-            
-            <div className='
-                    container 
-                    grid 
-                    grid-cols-2 
-                    text-white
-                    gambido-lipe
-                    '>
-                <div className="
-                        flex 
-                        flex-col 
-                        gap-4 
-                        items-center 
-                        justify-center 
-                        py-4
-                        ">
-                    <h2 className='
-                            text-5xl 
-                            font-bold
-                            text-white/80
-                            '>
-                        Seja bem vindos!
-                    </h2>
-                    <p className='text-xl text-white/80'  >
-                        Aqui você encontra os melhores:!
-                        </p>
+        <div className="bg-image-slide02 swiper-slide flex justify-center">
+  <div className='text-center items-center'>
+    <div className="flex flex-col pt-72 justify-items-center items-center gambido-lipe">
+      <h2 className='text-5xl items-center font-bold text-white/80'>
+        Mude o mundo, comece por você!
+      </h2>
+      <p className='text-2xl  my-1 text-white/80'>
+        Soluções energéticas para sua casa e sua empresa.
+      </p>
+      <div className="flex justify-around my-8">
+        <button className='rounded shadow-sm shadow-white/40 bg-transparent border border-white/70 text-white/70 hover:border-white/90 hover:text-white/90 py-2 px-4'>
+          Sobre Nos
+        </button>
+      </div>
+      {/* <div className="flex justify-around gap-4">
+        <Link to='/login' className='rounded bg-transparent border border-white/50 text-white/50 hover:border-white/80 hover:text-white/80 py-2 px-2'>
+          Entrar <br /> temporário
+        </Link>
+      </div> */}
+    </div>
+  </div>
+</div>
 
-                    <div className="flex justify-around gap-4">
-                        <button className='
-                                    rounded
-                                    bg-transparent
-                                    border
-                                    border-white/50
-                                    text-white/50
-
-                                    hover:border-white/80
-                                    hover:text-white/80
-                                    py-2 
-                                    px-4
-                                    '>
-                            Conheça mais
-                        </button>
-                    </div>
-                    <div className="flex justify-around gap-4">
-                    <Link to='/login' className='rounded
-                                    bg-transparent
-                                    border
-                                    border-white/50
-                                    text-white/50
-
-                                    hover:border-white/80
-                                    hover:text-white/80
-                                    py-2 px-2 '>Entrar <br /> temporario</Link>
-                    </div>
-                </div>
-
-                
-            </div>
-        </div>
     )
 }
 
